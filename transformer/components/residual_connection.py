595fea679281443dbdcbe2a7d182bbde94be5464
yuvraaj2002 <ys2002github@gmail.com>
2026-01-13 18:40:25 +0530

    Simplified the residual conneciton

--- transformer/components/residual_connection.py
+++ transformer/components/residual_connection.py
@@ -2,13 +2,13 @@ import torch
 from torch import nn
 from .layer_norm import LayerNormalization
 
+
 class ResidualConnection(nn.Module):
 
-    def __init__(self,embed_dim,epsilon,dropout,prev_layer):
+    def __init__(self, embed_dim, epsilon, dropout):
         super().__init__()
-        self.dropout_layer = nn.Dropout(dropout)
-        self.prev_layer = prev_layer
-        self.layer_norm = LayerNormalization(embed_dim,epsilon)
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization(embed_dim, epsilon)
 
-    def forward(self,x):
-        return x + self.dropout_layer(self.prev_layer(self.layer_norm(x)))
+    def forward(self, x, sublayer):
+        return self.norm(x + self.dropout(sublayer(x)))
